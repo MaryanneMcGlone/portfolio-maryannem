688a2ef25d0db0d81597e8006e88b84535f46838
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-07-04 22:26:35 -0400

    added App footer and styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <footer>
+        This portfolio was coded by{" "}
+        <a
+          href="https://github.com/inksp0ts09/ReactWeatherProject"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Maryanne McGlone
+        </a>
+      </footer>
     </div>
   );
 }
