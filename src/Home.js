970a1962310994731d522361644b2be4b928ad53
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-07-18 09:24:42 -0400

    Revert "Modified Navbar, created header component, modified home and app components"
    This reverts commit 32cb3f84986e03754179cf66d9f1424821384c11.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./Header.js";
 
 export default function Home() {
   <div className="Homepage">
     <div className="container-fluid">
-      <Header />
-      <div className="hero">Homepage</div>
+      <div className="hero"></div>
     </div>
   </div>;
 }
