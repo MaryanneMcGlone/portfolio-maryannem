750da553332bed060caa62415ccc04e74bd12a8f
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-07-27 09:10:26 -0400

    updated home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-import Navbar from "./Navbar.js";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <footer>
-        This portfolio was coded by{" "}
-        <a
-          href="https://github.com/inksp0ts09/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Maryanne M
-        </a>
-      </footer>
+      <div className="container">
+        <Home />
+        <footer>
+          This portfolio was coded by{" "}
+          <a
+            href="https://github.com/inksp0ts09/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maryanne M
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
-import logo from "./images/logoGit.png";
+import logo from "/images/logoGit.png";
+import Navbar from "./Navbar.js";
 
 export default function Home() {
   <div className="Homepage">
+    <Navbar />
     <div className="container-fluid">
       <div className="hero">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
